b2e51c68f214940ab269f9e23276f1e2fbca36eb
Vitor Dilucca <vitordilucca@hotmail.com>
2022-04-13 19:30:27 -0300

    correçao quando concluido a conta do imc os inputs sumir

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -57,52 +57,45 @@ export default function Form(props){
         
     }
 
-    return(
-            <View style={styles.formContext}>
-                {imc==null ?
-            <Pressable onPress={Keyboard.dismiss} style={styles.form}>
-
-                <Text style={styles.formLabel}>Altura</Text>
-                <Text style={styles.errorMessage}>{errorMessage}</Text>
-                <TextInput
-                    style={styles.input}
-                    onChangeText={setHeight}
-                    value={height}
-                    placeholder="Ex. 1.75"
-                    keyboardType="numeric"
-                />
-
-                <Text style={styles.formLabel}>Peso</Text>
-                <Text style={styles.errorMessage}>{errorMessage}</Text>
-                <TextInput
-                    style={styles.input}
-                    onChangeText={setWeight}
-                    value={weight}
-                    placeholder="Ex. 70.555"
-                    keyboardType="numeric"
-                />
-                <TouchableOpacity
-                    style={styles.buttonCalculator}
-                    onPress={() =>{
-                        validationImc()
-                }}
-                >
-                    <Text style={styles.textButtonCalculator}>{textButton}</Text>
-                </TouchableOpacity>
-                </Pressable>
-                : 
-                <View style={styles.exhibitionResultImc}>
-                    <ResultImc messageResultImc={messageImc} resultImc={imc}/>
-                    <TouchableOpacity
-                            style={styles.buttonCalculator}
-                            onPress={() =>{
-                                validationImc()
-                        }}
-                        >
-                            <Text style={styles.textButtonCalculator}>{textButton}</Text>
-                    </TouchableOpacity>
-                </View>
-            }
+    return (
+        <View style={styles.formContext}>
+        {imc == null ? 
+        <Pressable onPress={Keyboard.dismiss} style={styles.form}>
+            <Text style={styles.formLabel}>Altura</Text>
+            <Text style={styles.errorMessage}>{errorMessage}</Text>
+            <TextInput
+              style={styles.input}
+              onChangeText={setHeight}
+              value={height}
+              placeholder="Ex: 1.75"
+              keyboardType="numeric"
+            />
+            <Text style={styles.formLabel}>Peso</Text>
+            <Text style={styles.errorMessage}>{errorMessage}</Text>
+            <TextInput
+              style={styles.input}
+              onChangeText={setWeight}
+              value={weight}
+              placeholder="Ex: 86.300"
+              keyboardType="numeric"
+            />
+            <TouchableOpacity
+              style={styles.buttonCalculator}
+              onPress={() => {validationImc()}}
+            >
+              <Text style={styles.textButtonCalculator}>{textButton}</Text>
+            </TouchableOpacity>
+            </Pressable>
+        :
+        <View style={styles.exhibitionResultImc}>
+            <ResultImc messageResultIMc={messageImc} resultImc={imc} />
+            <TouchableOpacity
+                style={styles.buttonCalculator}
+                onPress={() =>{validationImc()}}
+            >
+                <Text style={styles.textButtonCalculator}>{textButton}</Text>
+            </TouchableOpacity>
         </View>
-    );
-}
+        }
+    </View>
+    );}
